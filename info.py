c3d3c31116f5ef62276bea7042e426b6e37f9c4c
Mahesh8188 <ddbambhava8182@gmail.com>
2025-02-01 21:59:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 # MongoDB information 
 # https://youtu.be/qFB0cFqiyOM?si=QGuFSZ7qhxl4VTrA
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://devashi624:devashi624@cluster0.01nzx.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://devvrat4517:devvrat4517@cluster0.c8f7j.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'lazyfilesx')
 
